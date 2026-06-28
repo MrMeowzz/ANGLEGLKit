37d82c6bfa14455eefa38aaa0a970e882d73148d
Mr Meowzz <41499772+MrMeowzz@users.noreply.github.com>
2026-06-27 22:08:27 -0700

    Update MGLDisplay.mm

--- MGLDisplay.mm
+++ MGLDisplay.mm
@@ -10,6 +10,7 @@
 #include <EGL/eglext.h>
 #include <EGL/eglext_angle.h>
 #include <EGL/eglplatform.h>
+#include <dlfcn.h>
 
 #ifndef EGL_PLATFORM_ANGLE_ANGLE
 #define EGL_PLATFORM_ANGLE_ANGLE 0x3202
@@ -33,23 +34,121 @@
 
 namespace
 {
+typedef __eglMustCastToProperFunctionPointerType (*MGLGetProcAddressProc)(const char *procname);
+typedef EGLDisplay (*MGLGetDisplayProc)(EGLNativeDisplayType display_id);
+typedef EGLBoolean (*MGLInitializeProc)(EGLDisplay dpy, EGLint *major, EGLint *minor);
+typedef EGLint (*MGLGetErrorProc)(void);
+typedef EGLBoolean (*MGLTerminateProc)(EGLDisplay dpy);
+
+static void *gLibEGLHandle = nullptr;
+static void *gLibGLESHandle = nullptr;
+static MGLGetProcAddressProc gGetProcAddress = nullptr;
+static MGLGetDisplayProc gGetDisplay = nullptr;
+static MGLInitializeProc gInitialize = nullptr;
+static MGLGetErrorProc gGetError = nullptr;
+static MGLTerminateProc gTerminate = nullptr;
+
 void Throw(NSString *msg)
 {
     [NSException raise:@"MGLSurfaceException" format:@"%@", msg];
 }
 
+static void *OpenFirstAvailableLibrary(const char *const paths[])
+{
+    for (int i = 0; paths[i] != nullptr; i++)
+    {
+        void *handle = dlopen(paths[i], RTLD_NOW | RTLD_GLOBAL);
+        if (handle)
+        {
+            NSLog(@"[ANGLEGLKit] Opened %s", paths[i]);
+            return handle;
+        }
+    }
+
+    NSLog(@"[ANGLEGLKit] dlopen failed. Last dlerror: %s", dlerror());
+    return nullptr;
+}
+
+static void EnsureEGLLibrariesLoaded()
+{
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        const char *eglPaths[] = {
+            "@loader_path/../libEGL.framework/libEGL",
+            "@rpath/libEGL.framework/libEGL",
+            "@executable_path/Frameworks/libEGL.framework/libEGL",
+            "libEGL.framework/libEGL",
+            nullptr
+        };
+
+        const char *glesPaths[] = {
+            "@loader_path/../libGLESv2.framework/libGLESv2",
+            "@rpath/libGLESv2.framework/libGLESv2",
+            "@executable_path/Frameworks/libGLESv2.framework/libGLESv2",
+            "libGLESv2.framework/libGLESv2",
+            nullptr
+        };
+
+        gLibEGLHandle = OpenFirstAvailableLibrary(eglPaths);
+        gLibGLESHandle = OpenFirstAvailableLibrary(glesPaths);
+
+        void *eglLookupHandle = gLibEGLHandle ? gLibEGLHandle : RTLD_DEFAULT;
+        gGetProcAddress = (MGLGetProcAddressProc)dlsym(eglLookupHandle, "eglGetProcAddress");
+        gGetDisplay = (MGLGetDisplayProc)dlsym(eglLookupHandle, "eglGetDisplay");
+        gInitialize = (MGLInitializeProc)dlsym(eglLookupHandle, "eglInitialize");
+        gGetError = (MGLGetErrorProc)dlsym(eglLookupHandle, "eglGetError");
+        gTerminate = (MGLTerminateProc)dlsym(eglLookupHandle, "eglTerminate");
+
+        NSLog(@"[ANGLEGLKit] EGL symbols: eglGetProcAddress=%p eglGetDisplay=%p eglInitialize=%p eglGetError=%p eglTerminate=%p",
+              gGetProcAddress, gGetDisplay, gInitialize, gGetError, gTerminate);
+    });
+}
+
+static void *ResolveEGLProc(const char *name)
+{
+    EnsureEGLLibrariesLoaded();
+
+    void *proc = nullptr;
+    if (gGetProcAddress)
+    {
+        proc = (void *)gGetProcAddress(name);
+    }
+
+    if (!proc && gLibEGLHandle)
+    {
+        proc = dlsym(gLibEGLHandle, name);
+    }
+
+    if (!proc)
+    {
+        proc = dlsym(RTLD_DEFAULT, name);
+    }
+
+    return proc;
+}
+
 static void LogEGLError(NSString *prefix)
 {
-    EGLint error = eglGetError();
-    NSLog(@"[ANGLEGLKit] %@ EGL error: 0x%04x", prefix, error);
+    EnsureEGLLibrariesLoaded();
+    if (gGetError)
+    {
+        EGLint error = gGetError();
+        NSLog(@"[ANGLEGLKit] %@ EGL error: 0x%04x", prefix, error);
+    }
+    else
+    {
+        NSLog(@"[ANGLEGLKit] %@ EGL error unavailable because eglGetError is missing.", prefix);
+    }
 }
 
 static EGLDisplay CreateMetalANGLEDisplay()
 {
+    EnsureEGLLibrariesLoaded();
+
     EGLDisplay display = EGL_NO_DISPLAY;
 
     PFNEGLGETPLATFORMDISPLAYEXTPROC getPlatformDisplayEXT =
-        (PFNEGLGETPLATFORMDISPLAYEXTPROC)eglGetProcAddress("eglGetPlatformDisplayEXT");
+        (PFNEGLGETPLATFORMDISPLAYEXTPROC)ResolveEGLProc("eglGetPlatformDisplayEXT");
 
     const EGLint extAttribs[] = {
         EGL_PLATFORM_ANGLE_TYPE_ANGLE, EGL_PLATFORM_ANGLE_TYPE_METAL_ANGLE,
@@ -86,7 +185,7 @@ static EGLDisplay CreateMetalANGLEDisplay()
     };
 
     PFNEGLGETPLATFORMDISPLAYPROC getPlatformDisplay =
-        (PFNEGLGETPLATFORMDISPLAYPROC)eglGetProcAddress("eglGetPlatformDisplay");
+        (PFNEGLGETPLATFORMDISPLAYPROC)ResolveEGLProc("eglGetPlatformDisplay");
 
     if (getPlatformDisplay)
     {
@@ -110,7 +209,13 @@ static EGLDisplay CreateMetalANGLEDisplay()
     }
 #endif
 
-    display = eglGetDisplay(EGL_DEFAULT_DISPLAY);
+    if (!gGetDisplay)
+    {
+        NSLog(@"[ANGLEGLKit] eglGetDisplay is missing.");
+        return EGL_NO_DISPLAY;
+    }
+
+    display = gGetDisplay(EGL_DEFAULT_DISPLAY);
 
     if (display != EGL_NO_DISPLAY)
     {
@@ -145,7 +250,12 @@ static EGLDisplay CreateMetalANGLEDisplay()
         EGLint major = 0;
         EGLint minor = 0;
 
-        if (!eglInitialize(_eglDisplay, &major, &minor))
+        if (!gInitialize)
+        {
+            Throw(@"Failed to resolve eglInitialize()");
+        }
+
+        if (!gInitialize(_eglDisplay, &major, &minor))
         {
             LogEGLError(@"eglInitialize failed");
             Throw(@"Failed to call eglInitialize()");
@@ -161,7 +271,11 @@ static EGLDisplay CreateMetalANGLEDisplay()
 {
     if (_eglDisplay != EGL_NO_DISPLAY)
     {
-        eglTerminate(_eglDisplay);
+        EnsureEGLLibrariesLoaded();
+        if (gTerminate)
+        {
+            gTerminate(_eglDisplay);
+        }
         _eglDisplay = EGL_NO_DISPLAY;
     }
 }
